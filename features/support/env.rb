eb94a18126c163decb1c30876b40c8d71acafdc5
Larry Cai <larry.caiyu@gmail.com>
2012-03-12 22:42:10 +0800

    support both for windows and unix platform

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,21 @@
 require 'aruba/cucumber'
-Before do
-    @dirs = ["/tmp/aruba"]
+
+# inspired from http://strong-leaf-44.heroku.com/2010/04/22/aruba---cucumber-goodness-for-the-command-line/
+# and updated with the information from source codes
+module ArubaOverrides
+  def detect_ruby(cmd)
+    processor, platform, *rest = RUBY_PLATFORM.split("-")
+    #puts platform
+    if platform =~ /w32$/ && cmd =~ /^mkbok /
+      "ruby -I../../lib -S ../../bin/#{cmd}"
+    else
+      "#{cmd}"
+    end
+  end
 end
+
+World(ArubaOverrides)
+
+#Before do
+#    @dirs = ["/tmp/aruba"]
+#end
